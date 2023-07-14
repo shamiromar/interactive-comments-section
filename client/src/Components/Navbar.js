34ef83a2a98da5a27870ded33db17891e9873457
“shamiromar” <shamiromar@yahoo.com>
2023-07-15 02:47:32 +0300

    Backend Test and CSS Start

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+function NavBar() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    };
+
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Forum">Forum</Nav.Link>
+          </Nav>
+          <Nav>
+            {isLoggedIn ? (
+                 <Button variant="info" size="sm" onClick={handleLogout}>Logout</Button>
+            ) : (
+                <Button variant="info" size="sm" onClick={handleLogin}>Login</Button>
+            ) }
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
